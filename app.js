242d5f717e2b76c4231ad47be78352e1a716cf21
tonymythical <174657016+tonymythical@users.noreply.github.com>
2026-02-18 20:16:20 -0800

    directed routing

--- app.js
+++ app.js
@@ -3,11 +3,32 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
+const guestbookUsers = [];
+
 app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.sendFile(`${import.meta.dirname}/views/home.html`);
 });
+
+app.post('/submit', (req, res) => {
+    const newEntry = {
+        name: req.body.name,
+        email: req.body.email,
+        message: req.body.message,
+        date: new Date().toLocaleString()
+    };
+
+    guestbookUsers.push(newEntry);
+
+    res.sendFile(`${import.meta.dirname}/views/index.html`);
+});
+
+app.get('/admin', (req, res) => {
+    res.json(guestbookUsers);
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
